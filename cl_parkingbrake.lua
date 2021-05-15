b2c8e191d3a1066ad6a79d838ba810b7494bbd89
supazorik <48525288+supazorik@users.noreply.github.com>
2021-05-15 13:48:43 -0700

    Added engagement of parking brake.

--- cl_parkingbrake.lua
+++ cl_parkingbrake.lua
@@ -1,72 +1,56 @@
-_KILL = false
+local engaged = false
+local firstTime = true
 
-
-local function createLock(engaged)
-    Citizen.CreateThread(function()
-        print("MADE", engaged)
-        while true do
-            Wait(0)
-            if _KILL == true then
-                _KILL = false
-                break
-            end
-            local ped, veh = PlayerPedId(), GetVehiclePedIsIn(ped, true)
-            
-                              
-            DisableControlAction(0, 71, true)
-            DisableControlAction(0, 72, true)
-
-            BringVehicleToHalt(veh, 10, 3, 0)
-
-            for i = 0, GetVehicleNumberOfWheels(veh) do
-                SetVehicleWheelBrakePressure(veh, i, 1000)
-            end
-        end
-    end)
-end
-
-AddEventHandler("Super:PBrake", function(engaged)
-    local ped, veh = PlayerPedId(), GetVehiclePedIsIn(PlayerPedId(), true)
-    local oldEngaged = true
-    
-    _KILL = true
-    Wait(500)
-    print("CALLED", engaged)
-    if engaged then
-        createLock(engaged)
-    else
-        --Citizen.CreateThread(function()
-            --[[while true do
-                Wait(0)
-                if _KILL then
-                    _KILL = false
-                    break
-                end]]
-            
-                DisableControlAction(0, 71, false)
-                DisableControlAction(0, 72, false)
-                EnableControlAction(0, 71, true)
-                EnableControlAction(0, 72, true)
-                for i = 0, GetVehicleNumberOfWheels(veh) do
-                    SetVehicleWheelBrakePressure(veh, i, 0)
-                end
-            --end
-        --end)
-    end
-
-end)
 
 Citizen.CreateThread(function()
-    local engaged = false
-
     while true do
         Wait(0)
         local ped = PlayerPedId()
 
         if IsControlJustReleased(0, 76) then
             engaged = not engaged
-            TriggerEvent("Super:PBrake", engaged)
+            firstTime = true
         end
     end
 end)
 
+Citizen.CreateThread(function()
+    ped, veh = PlayerPedId(), GetVehiclePedIsIn(PlayerPedId(), true)
+    local oldEngaged = true
+    while true do
+        Wait(0)
+
+        
+        if not engaged then
+            ped, veh = PlayerPedId(), GetVehiclePedIsIn(PlayerPedId(), true)
+            firstTime = false
+            DisableControlAction(0, 71, false)
+            DisableControlAction(0, 72, false)
+            EnableControlAction(0, 71, true)
+            EnableControlAction(0, 72, true)
+            SetVehicleBrake(veh, false)
+        end
+
+        if engaged then
+            local ped, veh = PlayerPedId(), GetVehiclePedIsIn(ped, true)
+            
+            if firstTime then
+                firstTime = false
+                BringVehicleToHalt(veh, 50, 50, -1)
+                DisableControlAction(0, 71, true)
+                DisableControlAction(0, 72, true)
+            end
+            
+            --[[
+            if oldEngaged ~= engaged then
+               BringVehicleToHalt(veh, 50, 50, 0)
+            end]]
+
+            
+
+            SetVehicleBrake(veh, true)
+        end
+        ped, veh = PlayerPedId(), GetVehiclePedIsIn(ped, true)
+        oldEngaged = engaged
+    end
+end)
