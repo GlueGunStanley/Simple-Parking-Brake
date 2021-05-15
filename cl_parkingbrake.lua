ff824940e503b3b683cd38dc7938e7a0cbafa627
supazorik <48525288+supazorik@users.noreply.github.com>
2021-05-15 03:03:26 -0700

    Added braking mechanism.

--- cl_parkingbrake.lua
+++ cl_parkingbrake.lua
@@ -1,5 +1,6 @@
+local engaged = false
+
 Citizen.CreateThread(function()
-    local engaged = false
     while true do
         Wait(50)
         local ped = PlayerPedId()
@@ -7,5 +8,28 @@ Citizen.CreateThread(function()
         if IsControlJustPressed(0, 76) then
             engaged = not engaged
         end
+    end
+end)
+
+Citizen.CreateThread(function()
+    local ped, veh = PlayerPedId(), GetVehiclePedIsIn(ped, true)
+    while true do
+        Wait(0)
+
+        if oldEngaged ~= engaged then
+            DisableControlAction(0, 71, false)
+            EnableControlACtion(0, 71, true)
+        end
+
+        if engaged then
+            local ped, veh = PlayerPedId(), GetVehiclePedIsIn(ped, true)
+
+
+            for i = 0, #GetVehicleNumberOfWheels(veh) do
+                SetVehicleWheelBrakePressure(veh, i, 100)
+                DisableControlAction(0, 71, true)
+            end
+        end
+        local oldEngaged = engaged
     end
 end)
