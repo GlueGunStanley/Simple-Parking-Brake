209096a6c27d63b614418f92f6fda9a8166dc6b1
supazorik <48525288+supazorik@users.noreply.github.com>
2021-05-15 02:52:22 -0700

    Added control check

--- cl_parkingbrake.lua
+++ cl_parkingbrake.lua
@@ -1,7 +1,11 @@
 Citizen.CreateThread(function()
+    local engaged = false
     while true do
+        Wait(50)
         local ped = PlayerPedId()
 
-        
+        if IsControlJustPressed(0, 76) then
+            engaged = not engaged
+        end
     end
 end)
