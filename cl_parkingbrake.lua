b5882220664a2366912db265f55fa24d4c6c5740
supazorik <48525288+supazorik@users.noreply.github.com>
2021-05-15 14:00:16 -0700

    Fixed everything by making it simple.

--- cl_parkingbrake.lua
+++ cl_parkingbrake.lua
@@ -9,10 +9,12 @@ Citizen.CreateThread(function()
 
         if IsControlJustReleased(0, 76) then
             engaged = not engaged
-            firstTime = true
+            SetVehicleHandbrake(veh, engaged)
+            -- firstTime = true
         end
     end
 end)
+
 
 Citizen.CreateThread(function()
     ped, veh = PlayerPedId(), GetVehiclePedIsIn(PlayerPedId(), true)
@@ -20,37 +22,8 @@ Citizen.CreateThread(function()
     while true do
         Wait(0)
 
+        SetVehicleHandbrake(veh, engaged)
+        SetVehicleBrake(veh, engaged)
         
-        if not engaged then
-            ped, veh = PlayerPedId(), GetVehiclePedIsIn(PlayerPedId(), true)
-            firstTime = false
-            DisableControlAction(0, 71, false)
-            DisableControlAction(0, 72, false)
-            EnableControlAction(0, 71, true)
-            EnableControlAction(0, 72, true)
-            SetVehicleBrake(veh, false)
-        end
-
-        if engaged then
-            local ped, veh = PlayerPedId(), GetVehiclePedIsIn(ped, true)
-            
-            if firstTime then
-                firstTime = false
-                BringVehicleToHalt(veh, 50, 50, -1)
-                DisableControlAction(0, 71, true)
-                DisableControlAction(0, 72, true)
-            end
-            
-            --[[
-            if oldEngaged ~= engaged then
-               BringVehicleToHalt(veh, 50, 50, 0)
-            end]]
-
-            
-
-            SetVehicleBrake(veh, true)
-        end
-        ped, veh = PlayerPedId(), GetVehiclePedIsIn(ped, true)
-        oldEngaged = engaged
     end
 end)
