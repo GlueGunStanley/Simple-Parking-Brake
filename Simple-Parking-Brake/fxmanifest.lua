3ca2a6eee37983db47c2edfe1f7a3335f624ac9c
GlueGunStanley <kasonstanley@gmail.com>
2024-12-12 05:49:29 -0500

    Created sv_functions.lua

--- Simple-Parking-Brake/fxmanifest.lua
+++ Simple-Parking-Brake/fxmanifest.lua
@@ -12,9 +12,11 @@ dependency "baseevents"
 
 client_scripts {
     'config.lua',
+    'client/cl_functions.lua',
     'client/cl_main.lua'
 }
 
 server_scripts {
+    'server/sv_functions.lua',
     'server/sv_main.lua'
 }
